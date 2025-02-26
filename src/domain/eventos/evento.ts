158947f8f16d746a1d304346440f9cfc632978c3
Deyvison Samuel Gomes do Nascimento <deyvisonnascimento2025@gmail.com>
2025-02-26 10:41:04 -0300

    commit evento

---
+++ src/domain/eventos/evento.ts
@@ -0,0 +1,18 @@
+export abstract class Evento {
+    protected timeStamp: number;
+    protected refeitorio: Refeitorio;
+    protected maquinaEventos: MaquinaDeEventos;
+
+    constructor(timeStamp: number, refeitorio: Refeitorio, maquinaDeEventos: MaquinaDeEventos) {
+        this.timeStamp = timeStamp;
+        this.refeitorio = refeitorio;
+        this.maquinaEventos = maquinaDeEventos;
+    }
+
+    public getTimeStamp(): number {
+        return this.timeStamp;
+    }
+
+    // Método abstrato que será implementado por classes concretas
+    abstract processarEvento(): void;
+}
