a936297e04efd242e2f7e2339a97cdebf5ebfe3a
M.Vihtória <mariavitoriasilvaaraujo326@gmail.com>
2025-02-26 01:03:09 -0300

    commit chamar aluno para catraca

---
+++ src/domain/classes/chamarAlunoParaCatraca.ts
@@ -0,0 +1,15 @@
+class ChamarAlunoParaCatraca extends Evento {
+    processarEvento(): void {
+        console.log(`Evento - ChamarAlunoParaCatraca - Time:${this.timeStamp}`);
+
+        // Move o aluno para a catraca e obtém o tempo necessário para digitar a matrícula
+        let alunoDaCatraca : Aluno = this.refeitorio.moverAlunoCatraca();
+        alunoDaCatraca.registrarChegadaNaCatraca(this.timeStamp)
+        let tempoDigitarMatricula : number = alunoDaCatraca.getTempoDigitarMatricula()
+        // Agenda o próximo evento para mover o aluno para a fila interna após o tempo de digitação
+        let agendamento: ChegadaAlunoFilaInterna;
+        let instanteChegadaFilaInterna = this.timeStamp + tempoDigitarMatricula;
+        agendamento = new ChegadaAlunoFilaInterna(instanteChegadaFilaInterna, this.refeitorio, this.maquinaEventos);
+        this.maquinaEventos.adicionarEvento(agendamento);
+    }
+}    
