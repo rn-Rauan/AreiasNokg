d86462eef76f97201a9f07bb12789db9167903b5
M.Vihtória <mariavitoriasilvaaraujo326@gmail.com>
2025-02-26 00:47:42 -0300

    commit serviço

--- src/domain/classes/repositoriodemesas.ts
+++ src/domain/classes/repositoriodemesas.ts
@@ -1,1 +1,25 @@
-M.vih.3107
+/**
+ * Classe RepositorioDeMesas que gerencia a criação e controle das mesas do refeitório.
+ */
+class RepositorioDeMesas {
+    mesa: Mesa[] = [];
+    quantidadeDeMesas: number;
+
+    constructor(quantidadeDeMesas: number) {
+        this.quantidadeDeMesas = quantidadeDeMesas;
+        if (quantidadeDeMesas <= 0) throw new Error('A quantidade de mesas não pode ser inferior ou igual a 0.');
+        this.createAllMesas(quantidadeDeMesas);
+    }
+
+    // Cria todas as mesas
+    private createAllMesas(quantidadeDeMesas: number): void {
+        for (let i: number = 0; i < quantidadeDeMesas; i++) {
+            this.mesa.push(this.createNewMesa());
+        }
+    }
+
+    // Cria uma nova mesa
+    private createNewMesa(): Mesa {
+        return new Mesa();
+    }
+}
